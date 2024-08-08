1fc51750284ac97d714642dc2062fa43510d6e70
Mndi-Iviwe <128236986+Mndi-Iviwe@users.noreply.github.com>
2024-08-08 10:54:54 +0200

    Add files via upload
    This is not the final draft of the website build. More changes are to be made.

---
+++ FANGJewellerz Website-20240610T160526Z-001/FANGJewellerz Website/Index.php
@@ -0,0 +1,58 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Styling/Style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+     integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+      crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Web Index</title>
+</head>
+<body>
+<section class="form-body">   
+    <section class="form-section">
+    <div class="form-logo">
+                <img src="Images/thumbnail_FangJewelerz.jpg" alt="FANG JEWLLERZ LOGO, CAUSE WE RUN THIS!!!">
+            </div>
+        <form action="HomePage.html">
+            <h1>Welcome To FANGJewellerz!!! Please Log-In.</h1>
+            <br>
+            <label for="email">E-mail</label><br>
+            <input type="text" name="email" require>
+            <br>
+            <br>
+            <label for="password">Password: </label><br>
+            <input type="password" name="password" required>
+            <br>
+            <br>
+            <input type="submit">
+            <p>You're new here, and don't have an account? <a href="RegistrationPage.html">click here</a> to Register!</p>
+        </form>
+    </section>
+</section>
+
+<footer>
+    <div class="footer-container">
+        <h2 style="color: white;">Window Shopping? Contact Use On our social media</h2>
+        <div class="social-icons">
+            <a href="https://za.pinterest.com/alexandriianyathii26/fangtastic-sa-pinspo/"><i class="fa-brands fa-pinterest"></i></a>
+            <a href="https://www.instagram.com/fangtasticsa?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://www.tiktok.com/discover/tooth-gem-fangs"><i class="fa-brands fa-tiktok"></i></a>
+        </div>
+        <nav>
+            <ul class="footer-nav-links">
+            <li><a href="HomePage.html">Homepage</a></li>
+            <li><a href="Products.html">Products</a></li>
+            <li><a href="NewsPage.html">News & Updates</a></li>
+            </ul>
+        </nav>
+        
+    </div>
+    <div class="footer-credit">
+        <p>@Mo Piiercings</p>
+        <p>155 Smit Street, Braamfontein</p>
+        <p>CopyRight &copy;2024; Designed By <span class="designer">FANGJewellerz</span></p>
+    </div>
+</footer>
+</body>
+</html>
